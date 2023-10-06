db0210825bb11657c04d595600f0cc95b6ac6213
Kevin Xiao <xiaoshun2005@gmail.com>
2023-10-06 17:02:03 -0400

    Fix width/height mismatching in Image

--- image.h
+++ image.h
@@ -23,9 +23,9 @@ struct Image {
         } else {
             /* See https://en.wikipedia.org/wiki/Netpbm#PPM_example */
             fout << "P3\n" << width << " " << height << "\n255\n";
-            for (ProgressBar<size_t> w(0, width, "Storing image as PPM"); w(); ++w) {
-                for (size_t h = 0; h < height; ++h) {
-                    fout << pixels[w][h].as_string() << '\n';
+            for (ProgressBar<size_t> h(0, height, "Storing PPM image"); h(); ++h) {
+                for (size_t w = 0; w < width; ++w) {
+                    fout << pixels[h][w].as_string() << '\n';
                 }
             }
 
@@ -33,27 +33,31 @@ struct Image {
         }
     }
 
+    /* --- NAMED CONSTRUCTORS --- **/
+
     /* Creates an image with width `w` and height `h` */
     static auto with_dimensions(size_t w, size_t h) {
         return Image{
             .width = w,
             .height = h,
-            .pixels = std::vector<std::vector<RGB>>(w, std::vector<RGB>(h))
+            .pixels = std::vector<std::vector<RGB>>(h, std::vector<RGB>(w))
         };
     }
 
     /* Creates an image with width `w` and aspect ratio (width / height) `aspect_ratio` */
     static auto with_width_and_aspect_ratio(size_t w, double aspect_ratio) {
+        auto h = static_cast<size_t>(std::round(static_cast<double>(w) / aspect_ratio));
         return with_dimensions(
             w, 
-            static_cast<size_t>(std::round(static_cast<double>(w) * aspect_ratio))
+            std::max(size_t{1}, h)   /* Make sure height is at least 1 */
         );
     }
 
     /* Creates an image with height `h` and aspect ratio (width / height) `aspect_ratio` */
     static auto with_height_and_aspect_ratio(size_t h, double aspect_ratio) {
+        auto w = static_cast<size_t>(std::round(static_cast<double>(h) * aspect_ratio));
         return with_dimensions(
-            static_cast<size_t>(std::round(static_cast<double>(h) / aspect_ratio)),
+            std::max(size_t{1}, w),  /* Make sure width is at least 1*/
             h
         );
     }
@@ -62,8 +66,8 @@ struct Image {
     Requires `img` to be a rectangular array. */
     static auto from_data(const std::vector<std::vector<RGB>> &img) {
         return Image {
-            .width = img.size(),
-            .height = img[0].size(),
+            .width = img[0].size(),
+            .height = img.size(),
             .pixels = img
         };
     }
