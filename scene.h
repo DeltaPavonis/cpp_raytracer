2eaada0efa18ced6e6ee1a3a79c9d4c39b51d2cf
Kevin Xiao <xiaoshun2005@gmail.com>
2023-10-12 10:49:29 -0400

    Add Scene type (an abstraction over a list of Hittable's)

---
+++ scene.h
@@ -0,0 +1,36 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <vector>
+#include <memory>
+#include "hittable.h"
+
+class Scene : public Hittable {
+    std::vector<std::shared_ptr<Hittable>> objects;
+
+public:
+
+    /* Add a object, stored within a `std::shared_ptr`, to the list of objects */
+    void add(std::shared_ptr<Hittable> object) {
+        /* Pass `shared_ptr` by copy, so this `Scene` will keep the object alive
+        as long as it itself has not been destroyed */
+        objects.push_back(object);
+    }
+
+    /* Return the `hit_info` from the closest object hit by the 3D ray `ray` */
+    hit_info hit_by(const Ray3D &ray, double t_min = 0,
+                double t_max = std::numeric_limits<double>::infinity()) const override {
+
+        hit_info result;
+        auto min_hit_time = t_max;
+        for (const auto &object : objects) {
+            if (auto curr = object->hit_by(ray, t_min, min_hit_time); curr) {
+                result = curr;
+                min_hit_time = curr.hit_time;
+            }
+        }
+        return result;
+    }
+};
+
+#endif
