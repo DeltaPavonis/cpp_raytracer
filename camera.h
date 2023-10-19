0fcf5b13c6bacea20e6d99bbd4220e11e9d4db7a
Kevin Xiao <xiaoshun2005@gmail.com>
2023-10-19 10:23:48 -0500

    Add Part 12: Adjustable Camera

--- camera.h
+++ camera.h
@@ -19,13 +19,24 @@ class Camera {
     double viewport_w{}, viewport_h{};
     /* The horizontal and vertical delta vectors from pixel to pixel. */
     Vec3D pixel_delta_x{}, pixel_delta_y{};
-    /* Coordinates of the camera/eye point and its distance away from the viewport,
-    by default (0, 0, 0) and 1, respectively. */
-    Point3D camera_center{};
-    double focal_length = 1;
-    /* Coordinates of the upper left corner of the viewport, and the coordinates of the
-    topmost and leftmost image pixel */
-    Point3D upper_left_corner{}, pixel00_loc{};
+    /* `camera` stores the camera ray; the coordinates of the camera/eye point,
+    and the direction in which the camera looks. `focal_length` is the distance of
+    the camera away from the viewport. */
+    Ray3D camera{.origin = Point3D{0, 0, 0}, .dir = Vec3D{0, 0, -1}};
+    double focal_length = std::numeric_limits<double>::infinity();
+    /* `view_up_dir` allows the user to specify the "up" direction for the camera.
+    Specifically, the "up" direction on the viewport is equal to the projection of
+    `view_up_dir` onto the viewport. By default, the "up" direction is the same as the 
+    positive y-axis. */
+    Vec3D view_up_dir{0, 1, 0};
+    /* `basis_x/y/z` form an orthonormal basis of the camera and its orientation.
+    `basis_x` is an unit vector pointing to the right, `basis_y` is an unit vector
+    pointing up (so `basis_x` and `basis_y` form an orthonormal basis for the viewport),
+    and `basis_z` is an unit vector pointing behind the camera, orthogonal to the viewport
+    (it poitns behind the camera and not in front due to the use of right-handed coordinates. )*/
+    Vec3D basis_x{}, basis_y{}, basis_z{};
+    /* Coordinates of the top-left image pixel */
+    Point3D pixel00_loc{};
     /* Number of rays sampled per pixel, 50 by default */
     size_t samples_per_pixel = 50;
     /* Maximum number of light ray bounces into the scene, 10 by default */
@@ -45,8 +56,12 @@ class Camera {
         because `image_w` and `image_h` both must be integers. */
         auto aspect_ratio = static_cast<double>(image_w) / static_cast<double>(image_h);
 
+        /* If `focal_length` is not explicitly provided by the user, then set it equal to
+        the length of the camera's direction vector. This is what is done in the tutorial. */
+        if (std::isinf(focal_length)) { focal_length = camera.dir.mag();}
+
         /* Set viewport_w and viewport_h based on `vertical_fov` or `horizontal_fov`,
-        and `aspect_ratio`. */
+        `focal_length`, and `aspect_ratio`. */
         if (!std::isinf(vertical_fov)) {
             viewport_h = 2 * focal_length * std::tan(vertical_fov / 2);
             viewport_w = viewport_h * aspect_ratio;
@@ -55,19 +70,25 @@ class Camera {
             viewport_h = viewport_w / aspect_ratio;
         }
 
+        /* Calculate an orthonormal basis for the camera and its orientation. */
+        basis_z = -camera.dir.unit_vector();
+        basis_x = cross(view_up_dir, basis_z).unit_vector();
+        basis_y = cross(basis_z, basis_x);  /* Always an unit vector because basis_z and x are */
+
         /* `x_vec` and `y_vec` are the vectors right and down across the viewport, respectively
         We use right-handed coordinates, which means the y-axis goes up, the x-axis goes right,
         and the NEGATIVE z-axis goes into the image. Thus, the vector going down across the
         viewport has a negative y-component. */
-        Vec3D x_vec{viewport_w, 0, 0}, y_vec{0, -viewport_h, 0};
+        Vec3D x_vec = viewport_w * basis_x, y_vec = -viewport_h * basis_y;
         pixel_delta_x = x_vec / static_cast<double>(image_w);  /* Divide by pixels per row */
         pixel_delta_y = y_vec / static_cast<double>(image_h);  /* Divide by pixels per column */
 
-        /* The upper left point of the camera is found by starting at the camera, moving
-        `focal_length` units towards the camera (so adding -`focal_length` to the z-coordinate
-        of `camera_center`, due to the use of right-handed coordinates), then subtracting half of
-        `x_vec` and `y_vec`. */
-        upper_left_corner = camera_center + Point3D{0, 0, -focal_length} - x_vec / 2 - y_vec / 2;
+        /* Find `upper_left_corner`, the coordinates of the upper left corner of the viewport.
+        The upper left point of the viewport is found by starting at the camera, moving
+        `focal_length` units towards the camera (so adding negative `focal_length` times
+        `basis_z` to `camera.origin`, due to the use of right-handed coordinates), then
+        subtracting half of `x_vec` and `y_vec`. */
+        auto upper_left_corner = camera.origin - focal_length * basis_z - x_vec / 2 - y_vec / 2;
 
         /* Pixels are inset from the edges of the camera by half the pixel-to-pixel distance.
         This ensures that the viewpoint area is evenly divided into `pixel_delta_x` by
@@ -75,7 +96,7 @@ class Camera {
         pixel00_loc = upper_left_corner + pixel_delta_x / 2 + pixel_delta_y / 2;
     }
 
-    /* Returns a ray from `camera_center` through a random point in the square region centered at
+    /* Returns a ray from `camera.origin` through a random point in the square region centered at
     the pixel in row `row` and column `col`. Note that the region is square because it is a
     rectangle with width |`pixel_delta_x`| and height |`pixel_delta_y`|, and we have `pixel_delta_x`
     = `x_vec` / `image_w` = `viewport_w` / `image_w`, and `pixel_delta_y` = `y_vec` / `image_h`
@@ -96,7 +117,7 @@ class Camera {
         a random real number in the range [-0.5, 0.5]. */
         auto pixel_sample = pixel_center + rand_double(-0.5, 0.5) * pixel_delta_x
                           + rand_double(-0.5, 0.5) * pixel_delta_y;
-        return Ray3D(camera_center, pixel_sample -camera_center);
+        return Ray3D(camera.origin, pixel_sample - camera.origin);
     }
 
 public:
@@ -151,9 +172,16 @@ public:
     /* Setters. Each returns a mutable reference to this object to create a functional interface */
 
     /* Sets the camera center to the point `p`. */
-    auto& set_camera_center(const Point3D &p) {camera_center = p; return *this;}
+    auto& set_camera_center(const Point3D &p) {camera.origin = p; return *this;}
+    /* Sets the camera direction to the vector `dir`. */
+    auto& set_camera_direction(const Vec3D &dir) {camera.dir = dir; return *this;}
+    /* Sets the point that the camera looks at from the camera center. */
+    auto& set_camera_lookat(const Point3D &p) {camera.dir = p - camera.origin; return *this;}
     /* Sets the focal length (the distance from the camera center to the viewport) to `focal_len`. */
     auto& set_focal_length(double focal_len) {focal_length = focal_len; return *this;}
+    /* Sets the "camera up" direction to the vector `dir`. The true camera up direction will be
+    determined by taking the projection of `dir` onto the viewport. */
+    auto& set_camera_up_direction(const Vec3D &dir) {view_up_dir = dir; return *this;}
     /* Sets the width of the final outputted image to `width`. */
     auto& set_image_width(size_t width) {image_w = width; return *this;}
     /* Sets the height of the final outputted image to `height`. */
@@ -165,14 +193,14 @@ public:
         return *this;
     }
     /* Sets the image width to `width`, and infers the image height from `width` and
-    `aspect_ratio` in `init()`.*/
+    `aspect_ratio`.*/
     auto& set_image_by_width_and_aspect_ratio(size_t width, double aspect_ratio) {
         auto height = static_cast<size_t>(std::round(static_cast<double>(width) / aspect_ratio));
         /* Make sure height is at least 1 */
         return set_image_dimensions(width, std::max(size_t{1}, height));
     }
     /* Sets the image height to `height`, and infers the image width from `height` and
-    `aspect_ratio` in `init()`. */
+    `aspect_ratio`. */
     auto& set_image_by_height_and_aspect_ratio(size_t height, double aspect_ratio) {
         auto width = static_cast<size_t>(std::round(static_cast<double>(height) * aspect_ratio));
         /* Make sure width is at least 1 */
@@ -184,14 +212,16 @@ public:
     a given light ray) to `max_depth_`. */
     auto& set_max_depth(size_t max_depth_) {max_depth = max_depth_; return *this;}
     /* Sets the vertical FOV (field of view) of the camera to `vertical_fov_degrees` degrees.
-    The horizontal FOV will then be inferred from `vertical_fov` and `aspect_ratio` in `init()`. */
+    The horizontal FOV will then be inferred from `vertical_fov` and the image's aspect ratio
+    in `init()`. */
     auto& set_vertical_fov(double vertical_fov_degrees) {
         vertical_fov = vertical_fov_degrees * std::numbers::pi / 180;
         horizontal_fov = std::numeric_limits<double>::infinity();
         return *this;
     }
     /* Sets the horizontal FOV (field of view) of the camera to `horizontal_fov_degrees` degrees.
-    The vertical FOV will then be inferred from `horizontal_fov` and `aspect_ratio` in `init()`. */
+    The vertical FOV will then be inferred from `horizontal_fov` and the image's `aspect_ratio`
+    in `init()`. */
     auto& set_horizontal_fov(double horizontal_fov_degrees) {
         horizontal_fov = horizontal_fov_degrees * std::numbers::pi / 180;
         vertical_fov = std::numeric_limits<double>::infinity();
--- ray3d.h
+++ ray3d.h
@@ -6,17 +6,17 @@
 #include "vec3d.h"
 
 struct Ray3D {
-    Point3D origin;
-    Vec3D dir;
+    Point3D origin = {0, 0, 0};
+    Vec3D dir = {0, 0, 0};
 
     /* Evaluate the ray at time `t` */
     auto operator() (double t) const {return origin + t * dir;}
 
     /* Constructs a ray with origin `origin_` and direction specified by `dir_` */
-    Ray3D(const Point3D &origin_, const Vec3D &dir_) : origin{origin_}, dir{dir_} {}
+    // Ray3D(const Point3D &origin_, const Vec3D &dir_) : origin{origin_}, dir{dir_} {}
 
     /* Default constructor results in a ray with origin and direction (0, 0, 0) */
-    Ray3D() : origin{0, 0, 0}, dir{0, 0, 0} {}
+    // Ray3D() : origin{0, 0, 0}, dir{0, 0, 0} {}
 };
 
 /* Overload `operator<<` to allow printing `Ray3D`s to output streams */
