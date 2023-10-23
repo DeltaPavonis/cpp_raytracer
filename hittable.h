DeltaPavonis/cpp_raytracer

commit 784daf558924f673cf557e4fc7369a6fb05d5708

Remove unused functions, improve comments, remove -Weffc++

hittable.h
@@ -15,28 +15,23 @@ class Material;
 
 struct hit_info {
 
-    /* `hit_time` = The time where the ray intersects an object. If the ray does
-    not intersect any object, then `hit_time` = `std::numeric_limits<double>::infinity()`. */
-    double hit_time = std::numeric_limits<double>::infinity();
+    /* `hit_time` = The time where the ray intersects an object */
+    double hit_time;
     /* `hit_point` = The point at which the ray intersects the object. If `ray`
     is the ray, then `hit_point` is equivalent to `ray(hit_time)`. */
-    Point3D hit_point{};
+    Point3D hit_point;
     /* `unit_surface_normal` = The unit vector normal to the surface at the point of intersection.
     `unit_surface_normal` points outward if the ray hit the outside of the surface, and it points
     inward if the ray hit the inside of the surface. Thus, `unit_surface_normal` represents the
     true unit surface normal at the hit point, taking into account whether the front or back face
     of the surface was hit. */
-    Vec3D unit_surface_normal{};
+    Vec3D unit_surface_normal;
     /* `hit_from_outside` = Whether or not the ray hit the outside of the surface. */
     bool hit_from_outside = false;  /* Named `front_face` in the tutorial */
     /* `material` = the `Material` of the object which the ray intersected */
-    std::shared_ptr<Material> material{};
+    std::shared_ptr<Material> material;
 
     /* --- CONSTRUCTORS ---*/
-
-    /* Default constructor denotes "no intersection"; `hit_time` is set to
-    `std::numeric_limits<double>::infinity()`. */
-    hit_info() {}
 
     /* Constructs a `hit_info` given `hit_time_` (the hit time), `hit_point_` (the point at
     which the ray intersects the surface), `outward_unit_surface_normal` (an UNIT VECTOR equalling
@@ -46,7 +41,6 @@ struct hit_info {
     Again, `outward_unit_surface_normal` is assumed to be an unit vector. */
     hit_info(double hit_time_, const Point3D &hit_point_, const Vec3D &outward_unit_surface_normal,
              const Ray3D &ray, std::shared_ptr<Material> material_)
-        : hit_time{hit_time_}, hit_point{hit_point_}, material{material_}
     {
         /* Determine, based on the directions of the ray and the outward surface normal at
         the ray's point of intersection, whether the ray was shot from inside the surface

Makefile
@@ -1,6 +1,6 @@
 CXX := g++-11
 
-CXXFLAGS := -std=c++20 -D_FILE_OFFSET_BITS=64 -Wall -Wextra -Wpedantic -Weffc++ -Wold-style-cast -Wshadow -Wcast-qual -Wwrite-strings -Wdisabled-optimization -Wfloat-equal -Wformat=2 -Wformat-overflow -Wformat-truncation -Wundef -fno-common -Wconversion -fdiagnostics-show-option -fdiagnostics-color=auto -Wuninitialized -Wno-unused-function -fstrict-aliasing -D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wno-unused -Wno-unused-result -Wno-unused-parameter -Wcast-align -Wnon-virtual-dtor -Woverloaded-virtual -Wmisleading-indentation -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wnull-dereference -Wuseless-cast -Wdouble-promotion
+CXXFLAGS := -std=c++20 -D_FILE_OFFSET_BITS=64 -Wall -Wextra -Wpedantic -Wold-style-cast -Wshadow -Wcast-qual -Wwrite-strings -Wdisabled-optimization -Wfloat-equal -Wformat=2 -Wformat-overflow -Wformat-truncation -Wundef -fno-common -Wconversion -fdiagnostics-show-option -fdiagnostics-color=auto -Wuninitialized -Wno-unused-function -fstrict-aliasing -D_FORTIFY_SOURCE=2 -fstack-protector-strong -Wno-unused -Wno-unused-result -Wno-unused-parameter -Wcast-align -Wnon-virtual-dtor -Woverloaded-virtual -Wmisleading-indentation -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wnull-dereference -Wuseless-cast -Wdouble-promotion
 # Consider adding -Wsign-conversion to the above
 CXXFLAGS += -MMD -MP
 CXXFLAGS += -I /home/kxiao/devel/competitive/kxlib # for kx.h

image.h
@@ -129,10 +129,11 @@ public:
                 int r, g, b;
                 if (!(fin >> r >> g >> b)) {
                     std::cout << "Error: In Image::from_ppm_file(\"" << file_name << "\"), failed "
-                                "to parse color #" << (row * image_width) + col + 1 << "color "
-                                "(three integers (r, g, b))" << std::endl;
+                                "to parse color #" << (row * image_width) + col + 1 <<
+                                " (three integers (r, g, b))" << std::endl;
                     std::exit(-1);
                 }
+                
                 ppm_data[row][col] = RGB::from_rgb(r, g, b, max_magnitude);
             }
         }
@@ -141,10 +142,11 @@ public:
     }
 };
 
-/* `ImagePPMStream` progressively prints the `RGB` pixels of an image with a specified width and
-height to a specified file. Unlike `Image`, it does not allow access to the pixels of the image,
-because it does not store the 2D array of pixels representing the image, which saves storage.
-Thus, for images that need post-processing, the `Image` type is more appropriate. */
+/* `ImagePPMStream` progressively takes in the `RGB` pixels of an image with a specified width and
+height, in the order of top to bottom then left to right, and prints those pixels to a specified
+file. Unlike `Image`, it does not allow access to the pixels of the image, because it does not store
+the 2D array of pixels representing the image, which saves storage. Thus, for images that need
+post-processing, the `Image` type is more appropriate. */
 class ImagePPMStream {
     std::string file;
     std::ofstream fout;

material.h
@@ -15,11 +15,6 @@ struct scatter_info {
     accounted for). TODO: Elaborate on why this color is named "attenuation". */
     RGB attenuation;
 
-    /* Converts to `true` if it represents a successful ray scattering, and false if not.
-    TODO: Determine when a ray can be UNsuccessfully scattered and decide on how that will be
-    indicated */
-    operator bool() {return true;}
-
     scatter_info(const Ray3D &ray_, const RGB &attenuation_)
         : ray{ray_}, attenuation{attenuation_} {}
 };

ray3d.h
@@ -2,16 +2,17 @@
 #define RAY3D_H
 
 #include <iostream>
-#include <cmath>
 #include "vec3d.h"
 
 /* `Ray3D` represents a ray in 3D space; that is, an origin (a 3D point) and
 a direction (a 3D vector). */
 struct Ray3D {
-    Point3D origin = {0, 0, 0};
-    Vec3D dir = {0, 0, 0};
+    /* `origin` = a 3D point representing the origin of the ray. (0, 0, 0) by default. */
+    Point3D origin{0, 0, 0};
+    /* `dir` = a 3D vector representing the direction of the ray. (0, 0, 0) by default. */
+    Vec3D dir{0, 0, 0};
 
-    /* Evaluate the ray at time `t` */
+    /* Returns the point located at time `t` on this ray */
     auto operator() (double t) const {return origin + t * dir;}
 };
 

rgb.h
@@ -5,7 +5,7 @@
 #include <string>
 #include "rand_util.h"
 
-/* Returns the gamma-encoded value of the magnitude `d`, under a gamma of `gamma`. */
+/* Returns the gamma-encoded value of the magnitude `d`, under a gamma of `gamma` (2 by default). */
 auto linear_to_gamma(double d, double gamma = 2) {
     /* See https://stackoverflow.com/a/16521337/12597781. */
     return std::pow(d, 1 / gamma);

sphere.h
@@ -32,9 +32,7 @@ struct Sphere : public Hittable {
 
     /* `Sphere::hit_by(ray)` returns a `hit_info` object representing the minimum time of
     intersection in the time range specified by `ray_times`, of `ray` with this Sphere. */
-    std::optional<hit_info> hit_by(const Ray3D &ray,
-                                   const Interval &ray_times = Interval::nonnegative())
-    const override {    
+    std::optional<hit_info> hit_by(const Ray3D &ray, const Interval &ray_times) const override {    
 
         /* Set up quadratic formula calculation */
         auto center_to_origin = ray.origin - center;

vec3d.h
@@ -25,21 +25,21 @@ struct Vec3D {
     /* Compute unit vector (forward declared) */
     Vec3D unit_vector() const;
 
-    /* Check if all three components less than `epsilon` */
+    /* Returns `true` if all three components have magnitude strictly less than `epsilon` */
     auto near_zero(double epsilon = 1e-8) {
         return (fabs(x) < epsilon) && (fabs(y) < epsilon) && (fabs(z) < epsilon);
     }
 
-    /* Generate random vector (by default, generates a vector with all components in [0, 1]) */
+    /* Generate random vector with real components in the interval [min, max] ([0, 1] by default) */
     static auto random(double min = 0, double max = 1) {
         return Vec3D{rand_double(min, max), rand_double(min, max), rand_double(min, max)};
     }
 
-    /* Generate random unit vector */
+    /* Generates an unformly random unit vector */
     static auto random_unit_vector() {
         /* Generate a random vector in the unit sphere, then normalize it (turn it into
         an unit vector). This ensures that each unit vector has a theoretically equal
-        probability of being generated. */
+        probability of being generated, unlike simply returning Vec3D::random(-1, 1). */
         Vec3D result;
         do {
             result = Vec3D::random(-1, 1);
@@ -75,7 +75,10 @@ auto operator* (const Vec3D &a, double d) {auto ret = a; ret *= d; return ret;}
 auto operator* (double d, const Vec3D &a) {return a * d;}
 auto operator/ (const Vec3D &a, double d) {auto ret = a; ret /= d; return ret;}
 
-/* Dot and cross product of two vectors*/
+/* Dot and cross product of two vectors. Note that these are not static because
+in OOP, static functions ought to not depend on the values of the member variables,
+or on the existence of actual instances of the class which they are a static member of.
+See https://softwareengineering.stackexchange.com/a/113034/426687. */
 auto dot(const Vec3D &a, const Vec3D &b) {return a.x * b.x + a.y * b.y + a.z * b.z;}
 auto cross(const Vec3D &a, const Vec3D &b) {
     return Vec3D{a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x};

commit 92c08395ae615be5bb4a132b16ab2a068a82d8a6

Re-add member init list to `Hittable::Hittable()`
Commit 784daf558924f673cf557e4fc7369a6fb05d5708 mistakenly removed the
member initialization list from `Hittable`'s default constructor,
causing it to leave the hit_time/hit_point/material fields
uninitialized. This led to segfaults when the uninitialized
`std::shared_ptr` that is material was dereferenced in `ray_color`.

hittable.h
@@ -41,6 +41,7 @@ struct hit_info {
     Again, `outward_unit_surface_normal` is assumed to be an unit vector. */
     hit_info(double hit_time_, const Point3D &hit_point_, const Vec3D &outward_unit_surface_normal,
              const Ray3D &ray, std::shared_ptr<Material> material_)
+        : hit_time{hit_time_}, hit_point{hit_point_}, material{material_}
     {
         /* Determine, based on the directions of the ray and the outward surface normal at
         the ray's point of intersection, whether the ray was shot from inside the surface