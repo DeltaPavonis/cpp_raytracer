4294b91aa010de45b8f06b15ccd2611b009f847f
Kevin Xiao <xiaoshun2005@gmail.com>
2023-10-28 15:29:58 -0400

    Change to using std::mt19937_64 in rand_double()
    For 64-bit doubles, using a 64-bit random number generator (such as
    std::mt19937_64) rather than what may be a 32-bit random number
    generator (such as std::mt19937) will likely result in better
    performance on modern processors (SIMD and other things). On both my
    machines, this change decreases the runtime of rendering the final
    "Ray Tracing in One Weekend" image by ~10%.

--- rand_util.h
+++ rand_util.h
@@ -6,7 +6,7 @@
 /* Generates an uniformly-random `double` in the range [`min`, `max`]
 (by default [0, 1]). */
 auto rand_double(double min = 0, double max = 1) {
-    thread_local std::mt19937 generator{std::random_device{}()};
+    thread_local std::mt19937_64 generator{std::random_device{}()};
     thread_local std::uniform_real_distribution<> dist;
     dist.param(std::uniform_real_distribution<>::param_type{min, max});
     return dist(generator);
