5658643d2107b92eb41485a353d9a41ef7136ec5
Kevin Xiao <xiaoshun2005@gmail.com>
2023-10-28 15:29:09 -0400

    Make distribution variable static in rand_double()
    Instead of constructing a new std::uniform_real_distribution in every
    call to rand_double(), we keep the distribution as a thread_local
    variable (which implies static), and set its minimum and maximum bounds
    each time rand_double() is called.

--- rand_util.h
+++ rand_util.h
@@ -7,7 +7,9 @@
 (by default [0, 1]). */
 auto rand_double(double min = 0, double max = 1) {
     thread_local std::mt19937 generator{std::random_device{}()};
-    return std::uniform_real_distribution<>{min, max}(generator);
+    thread_local std::uniform_real_distribution<> dist;
+    dist.param(std::uniform_real_distribution<>::param_type{min, max});
+    return dist(generator);
 }
 
 #endif
