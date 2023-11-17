3299115f0f4e6ea18c7187a519f720dd15b8069a
Kevin Xiao <xiaoshun2005@gmail.com>
2023-11-16 21:33:36 -0500

    Update Camera; use std::optional, add lookat field
    Remove the use of sentinel values for the Camera fields horizontal_fov,
    vertical_fov, and focus_dist by making them all std::optional's. Also,
    add the concept of a "lookat" point for the Camera (a point which the
    camera always points toward, no matter what the camera center is set
    to) and its corresponding setter function set_camera_lookat. The
    previous function with the same name is renamed to
    set_camera_direction_towards; see the code for comments.

--- camera.h
+++ camera.h
@@ -14,13 +14,20 @@ class Camera {
     /* Width and height (in pixels) of the final rendered image. 1280 x 720 by default */
     size_t image_w = 1280, image_h = 720;
     /* Width and height of the viewport (aka image plane). Note that these are real-valued.
-    Both are determined by either `vertical_fov` or `horizontal_fov` during `init()`. */
+    Both are determined by either `vertical_fov` or `horizontal_fov` (as well as the focal
+    length) during `init()`. */
     double viewport_w, viewport_h;
-    /* The horizontal and vertical delta vectors from pixel to pixel. */
+    /* The horizontal and vertical delta vectors from pixel to pixel in the viewport. */
     Vec3D pixel_delta_x, pixel_delta_y;
     /* `camera` stores the camera ray; the coordinates of the camera/eye point,
-    and the direction in which the camera looks. */
+    and the direction in which the camera looks. By default, the camera center is
+    at the origin, and the camera looks toward the direction of negative z-axis
+    (as is usual for right-handed coordinates, which we use). */
     Ray3D camera{.origin = Point3D{0, 0, 0}, .dir = Vec3D{0, 0, -1}};
+    /* `camera_lookat`, if specified, is the point towards which the camera always looks, no matter
+    where the camera center is. If not specified, then `camera.dir` will be used for the camera's
+    direction. */
+    std::optional<Point3D> camera_lookat;
     /* `view_up_dir` allows the user to specify the "up" direction for the camera.
     Specifically, the "up" direction on the viewport is equal to the projection of
     `view_up_dir` onto the viewport. By default, the "up" direction is the same as the 
@@ -41,9 +48,10 @@ class Camera {
     we will always place our viewport on the plane of perfect focus.
     
     `focus_dist` may be explicitly set by the user through `set_focus_distance`. If not set,
-    its value will be std::numeric_limits<double>>::infinity(), and the true focus distance
-    will be determined during `init()`. */
-    double focus_dist = std::numeric_limits<double>::infinity();
+    then the focus distance will default to the length of the camera's direction vector in
+    `init()` (that is, `focus_dist` defaults to `camera.dir.mag()` if not explicitly set).
+    The rationale behind this decision is explained in the comments for `init()`. */
+    std::optional<double> focus_dist;
     /* `defocus_angle` is the angle of the cone with apex at the viewport's center and circular
     base equivalent to the defocus disk (which is centered at the camera center). A `defocus_angle`
     of 0 represents no blur, and that is the default. `defocus_angle` is stored in radians. May
@@ -60,9 +68,13 @@ class Camera {
     /* Maximum number of light ray bounces into the scene, 10 by default */
     size_t max_depth = 10;
     /* Vertical and horizontal FOV (Field of View) of the camera, stored in radians.
-    By default, the vertical FOV is 90 degrees, and the horizontal FOV is determined
-    by the vertical FOV and the aspect ratio of the image  */
-    double vertical_fov = 90, horizontal_fov = std::numeric_limits<double>::infinity();
+    At any instant, only one of the vertical and horizontal FOV has a specified value
+    (only one of `vertical_fov` and `horizontal_fov` will be a non-empty `std::optional<double>`);
+    the other will be inferred from the FOV that is specified, as well as the image's aspect ratio,
+    in `init()`. By default, the vertical FOV is 90 degrees, and so the horizontal FOV will be
+    determined from the vertical FOVof 90 degrees  and the aspect ratio of the images in `init()`.
+    */
+    std::optional<double> vertical_fov{90}, horizontal_fov;
 
     /* Set the values of `viewport_w`, `viewport_h`, `pixel_delta_x`, `pixel_delta_y`,
     `upper_left_corner`, and `pixel00_loc` based on `image_w` and `image_h`. This function
@@ -74,23 +86,34 @@ class Camera {
         because `image_w` and `image_h` both must be integers. */
         auto aspect_ratio = static_cast<double>(image_w) / static_cast<double>(image_h);
 
+        /* If the user has explicitly provided a lookat point for the camera, then update
+        the camera's direction to be towards that lookat point from the current camera
+        center. */
+        if (camera_lookat) {
+            camera.dir = *camera_lookat - camera.origin;
+        }
+
         /* If `focus_dist` is not explicitly provided by the user, then set it equal to
-        the length of the camera's direction vector. */
-        if (std::isinf(focus_dist)) {focus_dist = camera.dir.mag();}
+        the length of the camera's direction vector. This way, if the user only specifies
+        a camera center and a camera lookat point, the focus distance will be so that the
+        lookat point is in perfect focus. */
+        if (!focus_dist) {
+            focus_dist = camera.dir.mag();
+        }
 
         /* The focal length (the distance from the camera center to the viewport) will always
         be set equal to the focus distance (the distance from the camera center to the plane of
         perfect focus). That is, we will always place our viewport on the plane of perfect
         focus. */
-        auto focal_length = focus_dist;
+        auto focal_length = *focus_dist;
 
         /* Set viewport_w and viewport_h based on `vertical_fov` or `horizontal_fov`,
         `focal_length`, and `aspect_ratio`. */
-        if (!std::isinf(vertical_fov)) {
-            viewport_h = 2 * focal_length * std::tan(vertical_fov / 2);
+        if (vertical_fov) {
+            viewport_h = 2 * focal_length * std::tan(*vertical_fov / 2);
             viewport_w = viewport_h * aspect_ratio;
         } else {
-            viewport_w = 2 * focal_length * std::tan(horizontal_fov / 2);
+            viewport_w = 2 * focal_length * std::tan(*horizontal_fov / 2);
             viewport_h = viewport_w / aspect_ratio;
         }
 
@@ -99,10 +122,10 @@ class Camera {
         cam_basis_x = cross(view_up_dir, cam_basis_z).unit_vector();
         cam_basis_y = cross(cam_basis_z, cam_basis_x); /* Unit vector because cam_basis_x/z are */
 
-        /* `x_vec` and `y_vec` are the vectors right and down across the viewport, respectively
-        We use right-handed coordinates, which means the y-axis goes up, the x-axis goes right,
-        and the NEGATIVE z-axis goes into the image. Thus, the vector going down across the
-        viewport has a negative y-component. */
+        /* `x_vec` and `y_vec` are the vectors right and down across the viewport (starting from the
+        top-left corner), respectively. We use right-handed coordinates, which means the y-axis goes
+        up, the x-axis goes right, and the NEGATIVE z-axis goes into the image. As a result, the
+        vector going down across the viewport has a negative y-component. */
         Vec3D x_vec = viewport_w * cam_basis_x, y_vec = -viewport_h * cam_basis_y;
         pixel_delta_x = x_vec / static_cast<double>(image_w);  /* Divide by pixels per row */
         pixel_delta_y = y_vec / static_cast<double>(image_h);  /* Divide by pixels per column */
@@ -120,9 +143,9 @@ class Camera {
         pixel00_loc = upper_left_corner + pixel_delta_x / 2 + pixel_delta_y / 2;
 
         /* Calculate the defocus disk radius vectors. To see how the calculation for
-        `defocus_disk_radius` works, remember that the defocus disk is the disk at the
-        bottom of the cone with apex at the center of the viewport, apex angle equal to
-        `defocus_angle`, and circular base centered at the camera center. */
+        `defocus_disk_radius` works, remember that the defocus disk is the base of the
+        right cone with apex at the center of the viewport, apex angle equal to `defocus_angle`,
+        and with its circular base centered at the camera center. */
         auto defocus_disk_radius = focal_length * std::tan(defocus_angle / 2);
         defocus_disk_x = defocus_disk_radius * cam_basis_x;
         defocus_disk_y = defocus_disk_radius * cam_basis_y;
@@ -140,7 +163,7 @@ class Camera {
     }
 
     /*
-    @brief Returns a ray originating from the defocus disk centered at `camera.origin`, and through
+    Returns a ray originating from the defocus disk centered at `camera.origin`, and through
     a random point in the square region centered at the pixel in row `row` and column `col`.
     
     Note that the region is square because it is a rectangle with width |`pixel_delta_x`| and
@@ -213,8 +236,8 @@ public:
 
         /* Calculate and store the color of each pixel */
         auto img = Image::with_dimensions(image_w, image_h);
-        
         ProgressBar pb(image_h, "Rendering and storing image");
+        
         #pragma omp parallel for
         for (size_t row = 0; row < image_h; ++row) {
             for (size_t col = 0; col < image_w; ++col) {
@@ -237,22 +260,44 @@ public:
     }
 
     /* Setters. Each returns a mutable reference to this object to create a functional interface */
-
-    /* Sets the camera center to the point `p`. */
+    
+    /* Sets the camera center to the point `p`. This is where the camera is placed. */
     auto& set_camera_center(const Point3D &p) {camera.origin = p; return *this;}
-    /* Sets the camera direction to the vector `dir`. */
+    /* Sets the camera direction to the vector `dir`.
+    
+    Note: If previously unset, then this Camera's focus distance will automatically be set to
+    the length of the direction vector `dir`. This way, objects placed at the end of the
+    camera's direction vector (or at other points that are the same distance away from the
+    camera center) will appear in perfect focus, and will linearly appear blurrier the further
+    away they are from that distance. */
     auto& set_camera_direction(const Vec3D &dir) {camera.dir = dir; return *this;}
     /* Sets the direction of the camera to be the direction from the camera center
-    to the point `p`. */
-    auto& set_camera_lookat(const Point3D &p) {camera.dir = p - camera.origin; return *this;}
+    to the point `p`. Note: this is NOT equivalent as `set_camera_lookat`; the latter
+    instructs the camera to always face toward the provided lookat point, whereas this
+    simply redirects the camera's current direction to face towards the provided point `p`.
+        
+    Note: If previously unset, then this Camera's focus distance will automatically be set to
+    the distance from the camera center to the point `p`. This way, objects placed at the point
+    `p` (and other points that are the same distance away from the camera center) will appear
+    in perfect focus, and will linearly appear blurrier the further away they are from that
+    distance. */
+    auto& set_camera_direction_towards(const Point3D &p) {
+        camera.dir = p - camera.origin;
+        return *this;
+    }
+    /* Set the camera direction to always be towards the point `p`, no matter what the camera
+    center is. */
+    auto& set_camera_lookat(const Point3D &p) {camera_lookat = p; return *this;}
     /* Set the focus distance (the distance from the camera center to the plane of perfect
-    focus). */
+    focus) to `focus_distance` units. Objects placed at that distance will appear in perfect
+    focus, and will linearly appear blurrier the further away they are from that distance. This
+    therefore allows for simulating defocus blur (depth-of-field) in the camera. */
     auto& set_focus_distance(double focus_distance) {focus_dist = focus_distance; return *this;}
     /* Set the defocus angle of the camera (the angle of the cone with apex at the center of the
     viewpoint and with base as the defocus disk, which is centered at the camera center) to
-    `defocus_angle_degrees` DEGREES, not radians. Note that setting the defocus angle to 0
-    eliminates all blur, making everything render in perfect focus; this can also be accomplished
-    by using `Camera::turn_blur_off()`. */
+    `defocus_angle_degrees` DEGREES, not radians. Smaller defocus angles result in less blur,
+    and setting the defocus angle to 0 eliminates all blur, making everything render in perfect
+    focus (which is equivalent to `Camera::turn_blur_off()`). */
     auto& set_defocus_angle(double defocus_angle_degrees) {
         defocus_angle = defocus_angle_degrees * std::numbers::pi / 180;  /* convert to radians */
         return *this;
@@ -286,6 +331,7 @@ public:
         /* Make sure width is at least 1 */
         return set_image_dimensions(std::max(size_t{1}, width), height);
     }
+
     /* Sets the number of rays sampled for each pixel to `samples`. */
     auto& set_samples_per_pixel(size_t samples) {samples_per_pixel = samples; return *this;}
     /* Sets the maximum recursive depth for the camera (the maximum number of bounces for
@@ -296,7 +342,7 @@ public:
     in `init()`. */
     auto& set_vertical_fov(double vertical_fov_degrees) {
         vertical_fov = vertical_fov_degrees * std::numbers::pi / 180;  /* convert to radians */
-        horizontal_fov = std::numeric_limits<double>::infinity();
+        horizontal_fov.reset();
         return *this;
     }
     /* Sets the horizontal FOV (field of view) of the camera to `horizontal_fov_degrees` degrees.
@@ -304,7 +350,7 @@ public:
     in `init()`. */
     auto& set_horizontal_fov(double horizontal_fov_degrees) {
         horizontal_fov = horizontal_fov_degrees * std::numbers::pi / 180;  /* convert to radians */
-        vertical_fov = std::numeric_limits<double>::infinity();
+        vertical_fov.reset();
         return *this;
     }
 };
